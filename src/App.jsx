025f4c5cfb05f6b9ff27a1545068a1a729387592
genarossi19 <genaros.gr@gmail.com>
2025-06-10 18:44:06 -0300

    Tarea 1: con if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Controles from './components/Controles'
-import Display from './components/Display'
+import { useState } from "react";
+import "./App.css";
+import Controles from "./components/Controles";
+import Display from "./components/Display";
 
 function App() {
   const [partido, setPartido] = useState({
     golesLocal: 0,
-    golesVisitante: 0
-  })
+    golesVisitante: 0,
+  });
 
   const onGol = (equipo) => {
-    setPartido({ ...partido, golesLocal: partido.golesLocal + 1 })
-  }
+    if (equipo === "local") {
+      setPartido({ ...partido, golesLocal: partido.golesLocal + 1 });
+    } else if (equipo === "visitante") {
+      setPartido({ ...partido, golesVisitante: partido.golesVisitante + 1 });
+    }
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Partido</h1>
       <Controles onGol={onGol} />
       <Display partido={partido} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
